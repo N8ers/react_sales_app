9b750dee1f6ab5ed3a04e2d94d1d0ad857a0d03f
N8ers <nathanraysheryak@gmail.com>
2018-08-07 13:21:53 -0400

    setup for logic

--- salesapp/src/Coursesales.js
+++ salesapp/src/Coursesales.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
 class Coursesales extends Component {
+    sumPrice(price){
+        this.setState({total: this.state.total + price});
+    }
+
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            total: 0
+        }; 
+        this.sumPrice = this.sumPrice.bind(this);
+    }
+
     render() {
         console.log(this.props.items);
-        
+        var courses = this.props.items.map((item, i) => {
+            return <Course name={item.name} price={item.price}
+                key={i} sumPrice={this.sumPrice} active={item.active} />
+        });
         return(
             <div>
-                I am from Coursesales page! :)
+                <h1>You can buy coures: </h1>
+                <div id="courses">{courses}</div>
+                <p id="total">total <b>{this.state.total}</b></p>
+            </div>
+        )
+    }
+}
+
+class Course extends Component {
+    render(){
+        return(
+            <div>
+
             </div>
         )
     }
